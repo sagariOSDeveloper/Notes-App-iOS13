5482fc6ccdf7d0dd069de87d9cec4d24f79cfa08
Sagar Ali <sagar18.iosdeveloper@gmail.com>
2020-08-09 20:56:14 +0500

    Table View Added with Navigation Bar

--- Notes App/ViewController.swift
+++ Notes App/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var myTableView:UITableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "Notes"
+        view.addSubview(myTableView)
+        myTableView.translatesAutoresizingMaskIntoConstraints = false
+        myTableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        myTableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        myTableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        myTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
-
-
 }
 
